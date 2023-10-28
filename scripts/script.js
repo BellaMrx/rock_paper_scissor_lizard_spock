db82602626e38d5c32a130c584087fb789c7db03
BellaMrx <bella.mrx.pr0ject@gmail.com>
2023-10-28 02:39:23 +0200

    UD

--- scripts/script.js
+++ scripts/script.js
@@ -1,139 +1,152 @@
-// legt user, computer score fest
+// sets user, computer score
 var uScore = 0;
 var cScore = 0;
 
-
-//funktion für button rock
+// function for button rock
 function btnRock() {
 	
-	//ändert bild bei spieler wahl durch button rock
-	var userImageChoice = document.querySelector("#displayPlayerChoice");
-	var computerImageResult = document.querySelector("#displayComputerChoice");
+	// changes image durcg icon at click button rock
+	var startImageRulesPlayer = document.querySelector("#displayPlayerStartImage");
+	var startImageRulesCom = document.querySelector("#displayComStartImage");
+
+    var weaponPlayer = document.querySelector("#iconPlayer");
+    var weaponCom = document.querySelector("#iconCom");
 	
-	//Animation
-	userImageChoice.style.animation = "shake 2s ease";
-	computerImageResult.style.animation = "shake 2s ease";
+	// animation
+	startImageRulesPlayer.style.animation = "shake 2s ease";
+	startImageRulesCom.style.animation = "shake 2s ease";
+    weaponPlayer.style.animation = "shake 2s ease";
+    weaponCom.style.animation = "shake 2s ease";
 	
-	//setzt timer bis zur anzeige
+	// sets timer until display
 	setTimeout (() => {
 		
-	//stoppt die animation
-	userImageChoice.removeAttribute("style");
-    computerImageResult.removeAttribute("style");
+	// stops the animation
+	startImageRulesPlayer.removeAttribute("style");
+    startImageRulesCom.removeAttribute("style");
+    weaponPlayer.removeAttribute("style");
+    weaponCom.removeAttribute("style");
 	
-	userImageChoice.src = '<i class="fa-solid fa-hand-back-fist"></i>';
+    startImageRulesPlayer.setAttribute("class", "hidden" );
+    startImageRulesCom.setAttribute("class", "hidden" );
+
+	weaponPlayer.innerHTML = '<i class="fa fa-hand-back-fist"></i>';
 	
-	//legt die 5 optionen fest
+	// sets the 5 options
 	var optionsR = [
 		{
-		  text: "Unentschieden",				//
-		  icon:  '<i class="fa-solid fa-hand-back-fist"></i>',		   		//option1
-		  score: "Draw",						//
+		  text: "Draw",				                            //
+		  icon:  '<i class="fa fa-hand-back-fist"></i>',        // option1
+		  score: "Draw",						                //
 		},
 		{
-		  text: "Computer gewinnt",				//
-		  icon:  '<i class="fa-solid fa-hand"></i>',			//option2
-		  score: "Lose",						//
+		  text: "Computer wins",				                //  
+		  icon:  '<i class="fa fa-hand"></i>',			        // option2
+		  score: "Lose",						                //
 		},
 		{
-		  text: "Du gewinnst",					//
-		  icon: '<i class="fa-solid fa-hand-scissors"></i>',			//option3
-		  score: "Win",							//
+		  text: "You win",					                    //
+		  icon: '<i class="fa fa-hand-scissors"></i>',			// option3
+		  score: "Win",							                //
 		},
 		{
-		  text: "Du gewinnst",					//
-          icon:  '<i class="fa-solid fa-hand-lizard"></i>',			//option4
-		  score: "Win",							//
+		  text: "You win",					                    //
+          icon:  '<i class="fa fa-hand-lizard"></i>',			// option4
+		  score: "Win",							                //
 		},
 		{
-		  text: "Computer gewinnt",				//
-          icon: '<i class="fa-solid fa-hand-spock"></i>',			//option5	
-		  score: "Lose",						//
+		  text: "Computer wins",				                //
+          icon: '<i class="fa fa-hand-spock"></i>',			    // option5	
+		  score: "Lose",						                //
 		}
 	];
 	
-	//wählt eine zufällige option aus
+	// selects a random(rnd) option
 	var rndR = optionsR[Math.floor(Math.random() * optionsR.length)];
 	
-	//gibt die ausgewälhte option aus
-	computerImageResult.src = rndR.icon;		//ausgabe option icon aus optionsR
+	// outputs the selected option
+	weaponCom.innerHTML = rndR.icon;		                    // output option icon from optionsR
 	
 	var computerTextResult = document.querySelector("#textResultDrawLoseWin");
-	computerTextResult.src = '<p>' + rndR.text + '</p>';		//ausgabe option text aus optionsR
+	computerTextResult.innerHTML = '<p>' + rndR.text + '</p>';	// output option text from optionsR
 	
-	userScore_span = document.getElementById("user-score");		//ausgabe spieler score ergebnis aus optionsR
-	computerScore_span = document.getElementById("computer-score");		//ausgabe computer score ergebnis aus optionsR
+	userScore_span = document.getElementById("user-score");		// output player score result from optionsR
+	computerScore_span = document.getElementById("computer-score");		// output computer score result from optionsR
 	if (rndR.score === "Draw") {
 		userScore_span.innerHTML = uScore;
 		computerScore_span.innerHTML = cScore;
 	} else if (rndR.score === "Lose") {
-		cScore++;		// addiert +1 zum computer score wenn option lose
-		computerScore_span.innerHTML = cScore;		//aktualisiert user score
+		cScore++;		// adds +1 to computer score when option lose
+		computerScore_span.innerHTML = cScore;		// updated user score
 	} else if (rndR.score === "Win") {
-		uScore++;		// addiert +1 zum user score wenn option win 
-		userScore_span.innerHTML = uScore;		//aktualisiert user score
+		uScore++;		// adds +1 to user score if option win 
+		userScore_span.innerHTML = uScore;		// updated user score
 	}
 	
-	//zeitangabe für timeout
+	// timeout indication
 	}, 2000);
 
 	return optionsR[rndR];
 }
 
 
-//funktion für button paper
+// function for button paper
 function btnPaper() {
 	
-	//ändert bild bei spieler wahl durch button paper
-	var userImageChoice = document.querySelector("#displayPlayerChoice");
-	var computerImageResult = document.querySelector("#displayComputerChoice");
+	var startImageRulesPlayer = document.querySelector("#displayPlayerStartImage");
+	var startImageRulesCom = document.querySelector("#displayComStartImage");
+
+    var weaponPlayer = document.querySelector("#iconPlayer");
+    var weaponCom = document.querySelector("#iconCom");
 	
-	//Animation
-	userImageChoice.style.animation = "shake 2s ease";
-	computerImageResult.style.animation = "shake 2s ease";
+	startImageRulesPlayer.style.animation = "shake 2s ease";
+	startImageRulesCom.style.animation = "shake 2s ease";
+    weaponPlayer.style.animation = "shake 2s ease";
+    weaponCom.style.animation = "shake 2s ease";
 	
-	//setzt timer bis zur anzeige
 	setTimeout (() => {
 		
-	//stoppt die animation
-	userImageChoice.removeAttribute("style");
-    computerImageResult.removeAttribute("style");
+	startImageRulesPlayer.removeAttribute("style");
+    startImageRulesCom.removeAttribute("style");
+    weaponPlayer.removeAttribute("style");
+    weaponCom.removeAttribute("style");
 	
-	userImageChoice.src = "RPSLSpaperUser.png";
+    startImageRulesPlayer.setAttribute("class", "hidden" );
+    startImageRulesCom.setAttribute("class", "hidden" );
+
+	weaponPlayer.innerHTML = '<i class="fa fa-hand"></i>';
 	
-	//legt die 5 optionen fest
 	var optionsP = [
 		{
-		  text: "Unentschieden",
-		  img:  "RPSLSpaperCom.png",
+		  text: "Draw",
+		  icon:  '<i class="fa fa-hand"></i>',
 		  score: "Draw",
 		},
 		{
-		  text: "Computer gewinnt",
-		  img:  "RPSLSscissorCom.png",
+		  text: "Computer wins",
+		  icon:  '<i class="fa fa-hand-scissors"></i>',
 		  score: "Lose",
 		},
 		{
-		  text: "Du gewinnst",
-		  img:  "RPSLSrockCom.png",
+		  text: "You win",
+		  icon:  '<i class="fa fa-hand-back-fist"></i>',
 		  score: "Win",
 		},
 		{
-		  text: "Computer gewinnt",
-          img:  "RPSLSlizardCom.png",		
+		  text: "Computer wins",
+          icon:  '<i class="fa fa-hand-lizard"></i>',		
 		  score: "Lose",
 		},
 		{
-		  text: "Du gewinnst",
-          img:  "RPSLSspockCom.png",		
+		  text: "You win",
+          icon:  '<i class="fa fa-hand-spock"></i>',		
 		  score: "Win",
 		}
 	];
 
-	//wählt eine zufällige option aus
 	var rndP = optionsP[Math.floor(Math.random() * optionsP.length)];
 	
-	computerImageResult.src = rndP.img;
+	weaponCom.innerHTML = rndP.icon;
 	
 	var computerTextResult = document.querySelector("#textResultDrawLoseWin");
 	computerTextResult.innerHTML = '<p>' + rndP.text + '</p>';
@@ -151,64 +164,68 @@ function btnPaper() {
 		userScore_span.innerHTML = uScore;
 	}
 	
-	//zeitangabe für timeout
 	}, 2000);
 	
 	return optionsP[rndP];
 }
 
 
-//funktion für button scissor
 function btnScissor() {
 	
-	//ändert bild bei spieler wahl durch button scissor
-	var userImageChoice = document.querySelector("#displayPlayerChoice");
-	var computerImageResult = document.querySelector("#displayComputerChoice");
+	var startImageRulesPlayer = document.querySelector("#displayPlayerStartImage");
+	var startImageRulesCom = document.querySelector("#displayComStartImage");
+
+    var weaponPlayer = document.querySelector("#iconPlayer");
+    var weaponCom = document.querySelector("#iconCom");
 	
-	//Animation
-	userImageChoice.style.animation = "shake 2s ease";
-	computerImageResult.style.animation = "shake 2s ease";
+	startImageRulesPlayer.style.animation = "shake 2s ease";
+	startImageRulesCom.style.animation = "shake 2s ease";
+    weaponPlayer.style.animation = "shake 2s ease";
+    weaponCom.style.animation = "shake 2s ease";
 	
-	//setzt timer bis zur anzeige
 	setTimeout (() => {
 		
-	//stoppt die animation
-	userImageChoice.removeAttribute("style");
-    computerImageResult.removeAttribute("style");
+	startImageRulesPlayer.removeAttribute("style");
+    startImageRulesCom.removeAttribute("style");
+    weaponPlayer.removeAttribute("style");
+    weaponCom.removeAttribute("style");
 	
-	userImageChoice.src = "RPSLSscissorUser.png";
+    startImageRulesPlayer.setAttribute("class", "hidden" );
+    startImageRulesCom.setAttribute("class", "hidden" );
+
+	weaponPlayer.innerHTML = '<i class="fa fa-hand-scissors"></i>';
 	
 	var optionsS = [
 		{
-		  text: "Unentschieden",
-		  img:  "RPSLSscissorCom.png",
+		  text: "Draw",
+		  icon:  '<i class="fa fa-hand-scissors"></i>',
 		  score: "Draw",
 		},
 		{
-		  text: "Computer gewinnt",
-		  img:  "RPSLSrockCom.png",
+		  text: "Computer wins",
+		  icon:  '<i class="fa fa-hand-back-fist"></i>',
 		  score: "Lose",
 		},
 		{
-		  text: "Du gewinnst",
-		  img:  "RPSLSpaperCom.png",
+		  text: "You win",
+		  icon:  '<i class="fa fa-hand"></i>',
 		  score: "Win",
 		},
 		{
-		  text: "Du gewinnst",
-          img:  "RPSLSlizardCom.png",		
+		  text: "You win",
+          icon:  '<i class="fa fa-hand-lizard"></i>',		
 		  score: "Win",
 		},
 		{
-		  text: "Du verlierst",
-          img:  "RPSLSspockCom.png",		
+		  text: "Computer wins",
+          icon:  '<i class="fa fa-hand-spock"></i>',		
 		  score: "Lose",
 		}		
 	];
 
 	var rndS = optionsS[Math.floor(Math.random() * optionsS.length)];
 	
-	computerImageResult.src = rndS.img;
+	weaponCom.innerHTML = rndS.icon;
 	
 	var computerTextResult = document.querySelector("#textResultDrawLoseWin");
 	computerTextResult.innerHTML = '<p>' + rndS.text + '</p>';
@@ -226,163 +243,167 @@ function btnScissor() {
 		userScore_span.innerHTML = uScore;
 	}
 	
-	//zeitangabe für timeout
 	}, 2000);
 	
 	return optionsS[rndS];
 }
 
 
-//funktion für button lizard
 function btnLizard() {
 	
-	//ändert bild bei spieler wahl durch button rock
-	var userImageChoice = document.querySelector("#displayPlayerChoice");
-	var computerImageResult = document.querySelector("#displayComputerChoice");
+	var startImageRulesPlayer = document.querySelector("#displayPlayerStartImage");
+	var startImageRulesCom = document.querySelector("#displayComStartImage");
+
+    var weaponPlayer = document.querySelector("#iconPlayer");
+    var weaponCom = document.querySelector("#iconCom");
 	
-	//Animation
-	userImageChoice.style.animation = "shake 2s ease";
-	computerImageResult.style.animation = "shake 2s ease";
+	startImageRulesPlayer.style.animation = "shake 2s ease";
+	startImageRulesCom.style.animation = "shake 2s ease";
+    weaponPlayer.style.animation = "shake 2s ease";
+    weaponCom.style.animation = "shake 2s ease";
 	
-	//setzt timer bis zur anzeige
 	setTimeout (() => {
 		
-	//stoppt die animation
-	userImageChoice.removeAttribute("style");
-    computerImageResult.removeAttribute("style");
+	startImageRulesPlayer.removeAttribute("style");
+    startImageRulesCom.removeAttribute("style");
 	
-	userImageChoice.src = "RPSLSlizardUser.png";
+    weaponPlayer.removeAttribute("style");
+    weaponCom.removeAttribute("style");
 	
-	//legt die 5 optionen fest
+    startImageRulesPlayer.setAttribute("class", "hidden" );
+    startImageRulesCom.setAttribute("class", "hidden" );
+
+	weaponPlayer.innerHTML = '<i class="fa fa-hand-lizard"></i>';
+	
 	var optionsL = [
 		{
-		  text: "Unentschieden",				//
-		  img:  "RPSLSlizardCom.png",		   	//option1
-		  score: "Draw",						//
+		  text: "Draw",				
+		  icon:  '<i class="fa fa-hand-lizard"></i>',
+		  score: "Draw",		
 		},
 		{
-		  text: "Computer gewinnt",				//
-		  img:  "RPSLSrockCom.png",				//option2
-		  score: "Lose",						//
+		  text: "Computer wins",			
+		  icon:  '<i class="fa fa-hand-back-fist"></i>',
+		  score: "Lose",		
 		},
 		{
-		  text: "Du gewinnst",					//
-		  img:  "RPSLSpaperCom.png",			//option3
-		  score: "Win",							//
+		  text: "You win",
+		  icon:  '<i class="fa fa-hand"></i>',	
+		  score: "Win",	
 		},
 		{
-		  text: "Du gewinnst",					//
-          img:  "RPSLSspockCom.png",			//option4 
-		  score: "Win",							//
+		  text: "You win",
+          icon:  '<i class="fa fa-hand-spock"></i>',
+		  score: "Win",	
 		},
 		{
-		  text: "Computer gewinnt",				//
-          img:  "RPSLSscissorCom.png",			//option5 	
-		  score: "Lose",						//
+		  text: "Computer wins",
+          icon:  '<i class="fa fa-hand-scissors"></i>',		
+		  score: "Lose",	
 		}
 	];
 	
-	//wählt eine zufällige option aus
 	var rndL = optionsL[Math.floor(Math.random() * optionsL.length)];
 	
-	//gibt die ausgewälhte option aus
-	computerImageResult.src = rndL.img;		//ausgabe option img aus optionsR
+	weaponCom.innerHTML = rndL.icon;
 	
 	var computerTextResult = document.querySelector("#textResultDrawLoseWin");
-	computerTextResult.innerHTML = '<p>' + rndL.text + '</p>';		//ausgabe option text aus optionsR
+	computerTextResult.innerHTML = '<p>' + rndL.text + '</p>';
 	
-	userScore_span = document.getElementById("user-score");		//ausgabe spieler score ergebnis aus optionsR
-	computerScore_span = document.getElementById("computer-score");		//ausgabe computer score ergebnis aus optionsR
+	userScore_span = document.getElementById("user-score");	
+	computerScore_span = document.getElementById("computer-score");	
 	if (rndL.score === "Draw") {
 		userScore_span.innerHTML = uScore;
 		computerScore_span.innerHTML = cScore;
 	} else if (rndL.score === "Lose") {
-		cScore++;		// addiert +1 zum computer score wenn option lose
-		computerScore_span.innerHTML = cScore;		//aktualisiert user score
+		cScore++;	
+		computerScore_span.innerHTML = cScore;		
 	} else if (rndL.score === "Win") {
-		uScore++;		// addiert +1 zum user score wenn option win 
-		userScore_span.innerHTML = uScore;		//aktualisiert user score
+		uScore++;		
+		userScore_span.innerHTML = uScore;		
 	}
 	
-	//zeitangabe für timeout
 	}, 2000);
 
 	return optionsL[rndL];
 }
 
 
-//funktion für button spock
 function btnSpock() {
 	
-	//ändert bild bei spieler wahl durch button rock
-	var userImageChoice = document.querySelector("#displayPlayerChoice");
-	var computerImageResult = document.querySelector("#displayComputerChoice");
+	var startImageRulesPlayer = document.querySelector("#displayPlayerStartImage");
+	var startImageRulesCom = document.querySelector("#displayComStartImage");
+
+    var weaponPlayer = document.querySelector("#iconPlayer");
+    var weaponCom = document.querySelector("#iconCom");
 	
-	//Animation
-	userImageChoice.style.animation = "shake 2s ease";
-	computerImageResult.style.animation = "shake 2s ease";
+	startImageRulesPlayer.style.animation = "shake 2s ease";
+	startImageRulesCom.style.animation = "shake 2s ease";
+    weaponPlayer.style.animation = "shake 2s ease";
+    weaponCom.style.animation = "shake 2s ease";
 	
-	//setzt timer bis zur anzeige
 	setTimeout (() => {
 		
-	//stoppt die animation
-	userImageChoice.removeAttribute("style");
-    computerImageResult.removeAttribute("style");
+	startImageRulesPlayer.removeAttribute("style");
+    startImageRulesCom.removeAttribute("style");
+    weaponPlayer.removeAttribute("style");
+    weaponCom.removeAttribute("style");
 	
-	userImageChoice.src = "RPSLSspockUser.png";
+    startImageRulesPlayer.setAttribute("class", "hidden" );
+    startImageRulesCom.setAttribute("class", "hidden" );
+
+	weaponPlayer.innerHTML = '<i class="fa fa-hand-spock"></i>';
 	
-	//legt die 5 optionen fest
 	var optionsSp = [
 		{
-		  text: "Unentschieden",				//
-		  img:  "RPSLSspockCom.png",		   	//option1
-		  score: "Draw",						//
+		  text: "Draw",				
+		  icon:  '<i class="fa fa-hand-spock"></i>',		   
+		  score: "Draw",		
 		},
 		{
-		  text: "Computer gewinnt",				//
-		  img:  "RPSLSpaperCom.png",			//option2
-		  score: "Lose",						//
+		  text: "Computer wins",		
+		  icon:  '<i class="fa fa-hand"></i>',		
+		  score: "Lose",					
 		},
 		{
-		  text: "Du gewinnst",					//
-		  img:  "RPSLSrockCom.png",				//option3
-		  score: "Win",							//
+		  text: "You win",				
+		  icon:  '<i class="fa fa-hand-back-fist"></i>',	
+		  score: "Win",							
 		},
 		{
-		  text: "Du gewinnst",					//
-          img:  "RPSLSscissorCom.png",			//option4 
-		  score: "Win",							//
+		  text: "You win",				
+          icon:  '<i class="fa fa-hand-scissors"></i>',			
+		  score: "Win",						
 		},
 		{
-		  text: "Computer gewinnt",				//
-          img:  "RPSLSlizardCom.png",			//option5 	
-		  score: "Lose",						//
+		  text: "Computer wins",				
+          icon:  '<i class="fa fa-hand-lizard"></i>',				
+		  score: "Lose",					
 		}
 	];
 	
-	//wählt eine zufällige option aus
+
 	var rndSp = optionsSp[Math.floor(Math.random() * optionsSp.length)];
 	
-	//gibt die ausgewälhte option aus
-	computerImageResult.src = rndSp.img;		//ausgabe option img aus optionsR
+	
+	weaponCom.innerHTML = rndSp.icon;		
 	
 	var computerTextResult = document.querySelector("#textResultDrawLoseWin");
-	computerTextResult.innerHTML = '<p>' + rndSp.text + '</p>';		//ausgabe option text aus optionsR
+	computerTextResult.innerHTML = '<p>' + rndSp.text + '</p>';		
 	
-	userScore_span = document.getElementById("user-score");		//ausgabe spieler score ergebnis aus optionsR
-	computerScore_span = document.getElementById("computer-score");		//ausgabe computer score ergebnis aus optionsR
+	userScore_span = document.getElementById("user-score");		
+	computerScore_span = document.getElementById("computer-score");		
 	if (rndSp.score === "Draw") {
 		userScore_span.innerHTML = uScore;
 		computerScore_span.innerHTML = cScore;
 	} else if (rndSp.score === "Lose") {
-		cScore++;		// addiert +1 zum computer score wenn option lose
-		computerScore_span.innerHTML = cScore;		//aktualisiert user score
+		cScore++;		
+		computerScore_span.innerHTML = cScore;	
 	} else if (rndSp.score === "Win") {
-		uScore++;		// addiert +1 zum user score wenn option win 
-		userScore_span.innerHTML = uScore;		//aktualisiert user score
+		uScore++;		
+		userScore_span.innerHTML = uScore;		
 	}
 	
-	//zeitangabe für timeout
 	}, 2000);
 
 	return optionsSp[rndSp];
